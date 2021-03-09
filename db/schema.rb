0198dcba9e866a636a48968404d7441571749eff
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-03-08 20:49:37 -0500

    add skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_014724) do
+ActiveRecord::Schema.define(version: 2021_03_09_014913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2021_03_09_014724) do
     t.string "job_title"
     t.string "company_name"
     t.string "details"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
