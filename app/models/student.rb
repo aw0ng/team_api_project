d8b4de00dc1fae8b60d7b9a83674489953a8f6b4
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-03-09 22:01:03 -0500

    add relationships for education, project and skill models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
 class Student < ApplicationRecord
   has_secure_password
   has_many :experiences
+  has_many :educations
+  has_many :skills
+  has_many :projects
 
   validates :email, presence: true, uniqueness: true
 end
