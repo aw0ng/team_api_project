de838a1bc9d2845aa94b97677258ec17fa1eecaf
sarahbaranski <sbaranski@gmail.com>
2021-03-09 21:28:52 -0600

    Added migrations to add student_id into skills and projects schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_010725) do
+ActiveRecord::Schema.define(version: 2021_03_10_032637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +43,14 @@ ActiveRecord::Schema.define(version: 2021_03_10_010725) do
     t.string "screenshot"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
     t.string "skill_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "student_id"
   end
 
   create_table "students", force: :cascade do |t|
