01bfa0d46bab00da2937b91166d9483bae4155f8
sarahbaranski <sbaranski@gmail.com>
2021-03-10 19:32:43 -0600

    Added show view and create, update, destroy to projects controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -3,4 +3,31 @@ class Api::ProjectsController < ApplicationController
     @projects = Project.all
     render "index.json.jb"
   end
+
+  def create
+    @project = Project.new(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot: params[:screenshot],
+    )
+    @project.save
+    render "show.json.jb"
+  end
+
+  def update
+    @project = Projecct.find_by(id: params[:id])
+    @project.name = params[:name] || @project.name
+    @project.description = params[:description] || @project.description
+    @project.url = params[:url] || @project.url
+    @project.screenshot = params[:screenshot] || @project.screenshot
+    @project.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    project = Project.find_by(id: params[:id])
+    project.destroy
+    render json: { message: "Project successfully destroyed." }
+  end
 end
